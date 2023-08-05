9173df5bfafb658f456b27be3cd2c0f656dc6b7d
Thumulurisirisha <141323361+Thumulurisirisha@users.noreply.github.com>
2023-08-05 15:04:55 +0530

    Update Demo2.java

--- src/com/upendra/Demo2.java
+++ src/com/upendra/Demo2.java
@@ -3,6 +3,10 @@ package com.upendra;
 public class Demo2 {
 	public static void main(String[] args) {
 		System.out.println("demo2");
+		System.out.println("demo2");
+		System.out.println("demo2");
+		System.out.println("demo2");
+		System.out.println("demo2");
 	}
 
 }
